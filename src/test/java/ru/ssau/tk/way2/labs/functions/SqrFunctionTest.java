f3de5eadbf544783cbdf417f16001fd5be2ea0a5
Kashapov <rusl.kashapov@gmail.com>
2021-09-22 19:01:26 +0400

    lab2 исправление

--- src/test/java/ru/ssau/tk/way2/labs/functions/SqrFunctionTest.java
+++ src/test/java/ru/ssau/tk/way2/labs/functions/SqrFunctionTest.java
@@ -12,19 +12,13 @@ public class SqrFunctionTest {
         double actual = first.apply(2);
         double expected = 4;
         assertEquals(actual, expected);
-    }
-    @Test
-    public void testApply1() {
-        SqrFunction first = new SqrFunction();
-        double actual = first.apply(-2);
-        double expected = 4;
-        assertEquals(actual, expected);
-    }
-    @Test
-    public void testApply2() {
-        SqrFunction first = new SqrFunction();
-        double actual = first.apply(0);
-        double expected = 0;
-        assertEquals(actual, expected);
+        SqrFunction second = new SqrFunction();
+        double actualA = second.apply(-2);
+        double expectedA = 4;
+        assertEquals(actualA, expectedA);
+        SqrFunction third = new SqrFunction();
+        double actualB = third.apply(0);
+        double expectedB = 0;
+        assertEquals(actualB, expectedB);
     }
 }
