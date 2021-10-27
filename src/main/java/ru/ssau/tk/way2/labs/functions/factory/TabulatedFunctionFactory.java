24b0be79be664baa4f5ca0253835e9ee2ee7a5c8
Kashapov <rusl.kashapov@gmail.com>
2021-10-27 17:44:46 +0400

    lab3

--- src/main/java/ru/ssau/tk/way2/labs/functions/factory/TabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/way2/labs/functions/factory/TabulatedFunctionFactory.java
@@ -1,7 +1,12 @@
 package ru.ssau.tk.way2.labs.functions.factory;
 
+import ru.ssau.tk.way2.labs.functions.StructTabulatedFunction;
 import ru.ssau.tk.way2.labs.functions.TabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
+
+    default TabulatedFunction createStrict(double[] xValues, double[] yValues) {
+        return new StructTabulatedFunction(create(xValues, yValues));
+    }
 }
