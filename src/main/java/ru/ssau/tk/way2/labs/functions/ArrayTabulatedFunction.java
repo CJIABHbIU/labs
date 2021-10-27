b577c81e8b3ddc910c02165b2753bf1684cc4213
Kashapov <rusl.kashapov@gmail.com>
2021-10-27 18:54:57 +0400

    lab3

--- src/main/java/ru/ssau/tk/way2/labs/functions/ArrayTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/way2/labs/functions/ArrayTabulatedFunction.java
@@ -2,11 +2,13 @@ package ru.ssau.tk.way2.labs.functions;
 
 import exceptions.InterpolationException;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class ArrayTabulatedFunction extends AbstractTabulatedFunction {
+public class ArrayTabulatedFunction extends AbstractTabulatedFunction implements Serializable {
+    private static final long serialVersionUID = -275918781697489264L;
     private final double[] x;
     private final double[] y;
 
--- src/main/java/ru/ssau/tk/way2/labs/functions/LinkedListTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/way2/labs/functions/LinkedListTabulatedFunction.java
@@ -2,11 +2,13 @@ package ru.ssau.tk.way2.labs.functions;
 
 import exceptions.InterpolationException;
 
+import java.io.Serializable;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class LinkedListTabulatedFunction extends AbstractTabulatedFunction {
+public class LinkedListTabulatedFunction extends AbstractTabulatedFunction implements Serializable {
 
+    private static final long serialVersionUID = 3822831438809733597L;
     private Node head;
     public static class Node {
         public Node next;
