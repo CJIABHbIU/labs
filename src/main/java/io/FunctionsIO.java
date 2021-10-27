5105bb12517ed8f88dc89b10ec5276bb5b583ac6
Kashapov <rusl.kashapov@gmail.com>
2021-10-27 18:06:51 +0400

    lab3

--- src/main/java/io/FunctionsIO.java
+++ src/main/java/io/FunctionsIO.java
@@ -1,7 +1,24 @@
 package io;
 
+import ru.ssau.tk.way2.labs.functions.Point;
+import ru.ssau.tk.way2.labs.functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException("Unavailable operation");
+    }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+        int i = 0;
+        for (Point a : function) {
+            printWriter.printf("%f %f\n", a.x, a.y);
+        }
+        printWriter.flush();
+
     }
 }
