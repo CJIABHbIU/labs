0b2a161d2acd0fb6fc049a45a01fc0daa579138d
Kashapov <rusl.kashapov@gmail.com>
2021-10-27 17:04:55 +0400

    lab3

--- src/main/java/operations/DifferentialOperator.java
+++ src/main/java/operations/DifferentialOperator.java
@@ -1,7 +1,8 @@
 package operations;
 
 import ru.ssau.tk.way2.labs.functions.MathFunction;
+import ru.ssau.tk.way2.labs.functions.TabulatedFunction;
 
-    public interface DifferentialOperator<T extends MathFunction> {
+public interface DifferentialOperator<T extends MathFunction> {
         T derive(T function);
-    }
+}
--- src/main/java/operations/TabulatedDifferentialOperator.java
+++ src/main/java/operations/TabulatedDifferentialOperator.java
@@ -1,9 +1,12 @@
 package operations;
 
+import ru.ssau.tk.way2.labs.functions.MathFunction;
+import ru.ssau.tk.way2.labs.functions.Point;
+import ru.ssau.tk.way2.labs.functions.TabulatedFunction;
 import ru.ssau.tk.way2.labs.functions.factory.ArrayTabulatedFunctionFactory;
 import ru.ssau.tk.way2.labs.functions.factory.TabulatedFunctionFactory;
 
-public class TabulatedDifferentialOperator {
+public class TabulatedDifferentialOperator implements DifferentialOperator<TabulatedFunction> {
     TabulatedFunctionFactory factory;
 
     public TabulatedDifferentialOperator() {
@@ -20,5 +23,24 @@ public class TabulatedDifferentialOperator {
 
     public TabulatedFunctionFactory getFactory() {
         return factory;
+    }
+    @Override
+    public TabulatedFunction derive(TabulatedFunction function) {
+
+        Point[] points = TabulatedFunctionOperationService.asPoints(function);
+        int length = points.length;
+
+        double[] xValues = new double[length];
+        double[] yValues = new double[length];
+
+        for (int i = 0; i < length - 1; i++) {
+            yValues[i] = (points[i + 1].y - points[i].y) / (points[i + 1].x - points[i].x);
+            xValues[i] = points[i].x;
+        }
+
+        yValues[length - 1] = yValues[length - 2];
+        xValues[length - 1] = points[length - 1].x;
+
+        return factory.create(xValues, yValues);
     }
 }
