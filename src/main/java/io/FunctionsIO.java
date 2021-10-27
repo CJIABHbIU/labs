36f3c5f2792d6c7ea3f7a8916e78d242446164ac
Kashapov <rusl.kashapov@gmail.com>
2021-10-27 18:16:50 +0400

    lab3

--- src/main/java/io/FunctionsIO.java
+++ src/main/java/io/FunctionsIO.java
@@ -3,8 +3,7 @@ package io;
 import ru.ssau.tk.way2.labs.functions.Point;
 import ru.ssau.tk.way2.labs.functions.TabulatedFunction;
 
-import java.io.BufferedWriter;
-import java.io.PrintWriter;
+import java.io.*;
 
 public final class FunctionsIO {
     private FunctionsIO() {
@@ -19,6 +18,14 @@ public final class FunctionsIO {
             printWriter.printf("%f %f\n", a.x, a.y);
         }
         printWriter.flush();
-
+    }
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream out = new DataOutputStream(outputStream);
+        out.writeInt(function.getCount());
+        for (Point point : function) {
+            out.writeDouble(point.x);
+            out.writeDouble(point.y);
+        }
+        out.flush();
     }
 }
