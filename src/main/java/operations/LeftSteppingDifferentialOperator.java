3ed45ea45d5084b48266b39cbd099918668bdd64
Kashapov <rusl.kashapov@gmail.com>
2021-10-27 17:41:21 +0400

    lab3

---
+++ src/main/java/operations/LeftSteppingDifferentialOperator.java
@@ -0,0 +1,14 @@
+package operations;
+
+import ru.ssau.tk.way2.labs.functions.MathFunction;
+
+public class LeftSteppingDifferentialOperator extends SteppingDifferentialOperator{
+    public LeftSteppingDifferentialOperator(double step) {
+        super(step);
+    }
+
+    @Override
+    public MathFunction derive(MathFunction func) {
+        return x -> (func.apply(x) - func.apply(x - step)) / step;
+    }
+}
---
+++ src/main/java/operations/MiddleSteppingDifferentialOperator.java
@@ -0,0 +1,14 @@
+package operations;
+
+import ru.ssau.tk.way2.labs.functions.MathFunction;
+
+public class MiddleSteppingDifferentialOperator extends SteppingDifferentialOperator{
+    public MiddleSteppingDifferentialOperator(double step) {
+        super(step);
+    }
+
+    @Override
+    public MathFunction derive(MathFunction func) {
+        return x -> (func.apply(x + step) - func.apply(x - step)) / (2 * step);
+    }
+}
