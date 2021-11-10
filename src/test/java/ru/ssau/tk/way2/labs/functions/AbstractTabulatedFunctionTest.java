1bae81d10de5660007bfaaa7b12d1a39991a5cf9
Kashapov <rusl.kashapov@gmail.com>
2021-11-11 03:02:11 +0400

    lab3

--- src/test/java/ru/ssau/tk/way2/labs/functions/AbstractTabulatedFunctionTest.java
+++ src/test/java/ru/ssau/tk/way2/labs/functions/AbstractTabulatedFunctionTest.java
@@ -7,6 +7,28 @@ import ru.ssau.tk.way2.labs.exceptions.DifferentLengthOfArraysException;
 import static org.testng.Assert.*;
 
 public class AbstractTabulatedFunctionTest {
+    private static final double ACCURACY = 0.001;
+    MockTabulatedFunction mock = new MockTabulatedFunction();
+
+    @Test
+    public void testInterpolate() {
+
+        assertEquals(mock.interpolate(2.0, 1.0, 5.0, 4.0, 2.0), 3.5, ACCURACY);
+        assertEquals(mock.interpolate(5.0, 3.0, 7.0, 6.0, 8.0), 7, ACCURACY);
+        assertEquals(mock.interpolate(3.0, 2.71828182854, 3.1415926535, 3.1415926535, 2.71828182854), 2.859874482040000, ACCURACY);
+        assertEquals(mock.interpolate(9.0, 2.71828182854 * 2.71828182854, 3.1415926535 * 3.1415926535, 3.1415926535, 2.71828182854), 2.866681662562116, ACCURACY);
+        assertEquals(mock.interpolate(2.0, 1.0, 3.0, 10.0, 10.0), 10, ACCURACY);
+    }
+
+    @Test
+    public void applyTest() {
+        assertEquals(mock.apply(5), 2.0, ACCURACY);
+        assertEquals(mock.apply(7), 1.0, ACCURACY);
+        assertEquals(mock.apply(3), 3.0, ACCURACY);
+        assertEquals(mock.apply(1), 4.0, ACCURACY);
+        assertEquals(mock.apply(0), 4.5, ACCURACY);
+    }
+
     @Test
     public void testCheckLengthIsTheSame() {
         assertThrows(DifferentLengthOfArraysException.class, () -> {
@@ -18,6 +40,7 @@ public class AbstractTabulatedFunctionTest {
         double[] y = new double[]{1, 16, 81, 256, 22};
         AbstractTabulatedFunction.checkLengthIsTheSame(x, y);
     }
+
     @Test
     public void testCheckSorted() {
         assertThrows(ArrayIsNotSortedException.class, () -> {
---
+++ src/test/java/ru/ssau/tk/way2/labs/functions/MockTabulatedFunction.java
@@ -0,0 +1,94 @@
+package ru.ssau.tk.way2.labs.functions;
+
+import java.util.Iterator;
+
+public class MockTabulatedFunction extends AbstractTabulatedFunction {
+    final double x0 = 1.0;
+    final double x1 = 5.0;
+    final double y0 = 4.0;
+    final double y1 = 2.0;
+
+    protected int floorIndexOfX(double x) {
+        if (x < x0) {
+            return 0;
+
+        } else if (x >= x0 && x <= x1) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    protected double extrapolateLeft(double x) {
+        return interpolate(x, x0, x1, y0, y1);
+    }
+
+    protected double extrapolateRight(double x) {
+        return interpolate(x, x0, x1, y0, y1);
+    }
+
+    protected double interpolate(double x, int floorIndex) {
+        return interpolate(x, x0, x1, y0, y1);
+    }
+
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public double getX(int index) {
+        if (index == 1) {
+            return x1;
+        }
+        return x0;
+    }
+
+    @Override
+    public double getY(int index) {
+        if (index == 1) {
+            return y1;
+        }
+        return y0;
+    }
+
+    @Override
+    public void setY(int index, double value) {
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        if (x == x1) {
+            return 1;
+        }
+        if (x == x0) {
+            return 0;
+        }
+        return -1;
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        if (y == y1) {
+            return 1;
+        }
+        if (y == y0) {
+            return 0;
+        }
+        return -1;
+    }
+
+    @Override
+    public double leftBound() {
+        return x0;
+    }
+
+    @Override
+    public double rightBound() {
+        return x1;
+    }
+
+    @Override
+    public Iterator<Point> iterator() {
+        return null;
+    }
+}
