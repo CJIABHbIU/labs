ed2558171486069d18938077b7d466e7b4fdd8bd
Kashapov <rusl.kashapov@gmail.com>
2021-11-30 21:23:12 +0400

    lab3

--- src/test/java/ru/ssau/tk/way2/labs/functions/LinkedListTabulatedFunctionTest.java
+++ src/test/java/ru/ssau/tk/way2/labs/functions/LinkedListTabulatedFunctionTest.java
@@ -47,7 +47,7 @@ public class LinkedListTabulatedFunctionTest {
 
         LinkedListTabulatedFunction list2 = new LinkedListTabulatedFunction(function, 1, 3, 10);
 
-        assertThrows(IllegalArgumentException.class, ()->{
+        assertThrows(IllegalArgumentException.class, () -> {
             LinkedListTabulatedFunction list = new LinkedListTabulatedFunction(function, 1, 1, 1);
         });
 
@@ -268,64 +268,65 @@ public class LinkedListTabulatedFunctionTest {
         assertEquals(a.getY(1), 100);
         assertThrows(IllegalArgumentException.class, () -> a.remove(0));
     }
+
     @Test
-    public  void testInsert(){
-    LinkedListTabulatedFunction t11 = new LinkedListTabulatedFunction(new double[]{1, 4, 9, 16}, new double[]{1, 16, 81, 256});
+    public void testInsert() {
+        LinkedListTabulatedFunction t11 = new LinkedListTabulatedFunction(new double[]{1, 4, 9, 16}, new double[]{1, 16, 81, 256});
         t11.insert(4, 2);
         System.out.println(t11);
-    assertEquals(t11.getX(0), 1.0);
-    assertEquals(t11.getY(0), 1.0);
-    assertEquals(t11.getX(1), 4.0);
-    assertEquals(t11.getY(1), 2.0);
-    assertEquals(t11.getX(2), 9.0);
-    assertEquals(t11.getY(2), 81.0);
-    assertEquals(t11.getX(3), 16.0);
-    assertEquals(t11.getY(3), 256.0);
-    assertEquals(t11.getCount(), 4);
+        assertEquals(t11.getX(0), 1.0);
+        assertEquals(t11.getY(0), 1.0);
+        assertEquals(t11.getX(1), 4.0);
+        assertEquals(t11.getY(1), 2.0);
+        assertEquals(t11.getX(2), 9.0);
+        assertEquals(t11.getY(2), 81.0);
+        assertEquals(t11.getX(3), 16.0);
+        assertEquals(t11.getY(3), 256.0);
+        assertEquals(t11.getCount(), 4);
         t11.insert(5, 6);
         System.out.println(t11);
-    assertEquals(t11.getX(0), 1.0);
-    assertEquals(t11.getY(0), 1.0);
-    assertEquals(t11.getX(1), 4.0);
-    assertEquals(t11.getY(1), 2.0);
-    assertEquals(t11.getX(2), 5.0);
-    assertEquals(t11.getY(2), 6.0);
-    assertEquals(t11.getX(3), 9.0);
-    assertEquals(t11.getY(3), 81.0);
-    assertEquals(t11.getX(4), 16.0);
-    assertEquals(t11.getY(4), 256.0);
-    assertEquals(t11.getCount(), 5);
-    LinkedListTabulatedFunction t1 = new LinkedListTabulatedFunction(new double[]{1, 4, 9, 16}, new double[]{1, 16, 81, 256});
+        assertEquals(t11.getX(0), 1.0);
+        assertEquals(t11.getY(0), 1.0);
+        assertEquals(t11.getX(1), 4.0);
+        assertEquals(t11.getY(1), 2.0);
+        assertEquals(t11.getX(2), 5.0);
+        assertEquals(t11.getY(2), 6.0);
+        assertEquals(t11.getX(3), 9.0);
+        assertEquals(t11.getY(3), 81.0);
+        assertEquals(t11.getX(4), 16.0);
+        assertEquals(t11.getY(4), 256.0);
+        assertEquals(t11.getCount(), 5);
+        LinkedListTabulatedFunction t1 = new LinkedListTabulatedFunction(new double[]{1, 4, 9, 16}, new double[]{1, 16, 81, 256});
         System.out.println(t1);
         t1.insert(0, 10);
         System.out.println(t1);
         t11.insert(5, 6);
         System.out.println(t11);
-    assertEquals(t1.getX(0), 0.0);
-    assertEquals(t1.getY(0), 10.0);
-    assertEquals(t1.getX(1), 1.0);
-    assertEquals(t1.getY(1), 1.0);
-    assertEquals(t1.getX(2), 4.0);
-    assertEquals(t1.getY(2), 16.0);
-    assertEquals(t1.getX(3), 9.0);
-    assertEquals(t1.getY(3), 81.0);
-    assertEquals(t1.getX(4), 16.0);
-    assertEquals(t1.getY(4), 256.0);
-    assertEquals(t1.getCount(), 5);
+        assertEquals(t1.getX(0), 0.0);
+        assertEquals(t1.getY(0), 10.0);
+        assertEquals(t1.getX(1), 1.0);
+        assertEquals(t1.getY(1), 1.0);
+        assertEquals(t1.getX(2), 4.0);
+        assertEquals(t1.getY(2), 16.0);
+        assertEquals(t1.getX(3), 9.0);
+        assertEquals(t1.getY(3), 81.0);
+        assertEquals(t1.getX(4), 16.0);
+        assertEquals(t1.getY(4), 256.0);
+        assertEquals(t1.getCount(), 5);
         t1.insert(27, 1010);
         System.out.println(t1);
-    assertEquals(t1.getX(0), 0.0);
-    assertEquals(t1.getY(0), 10.0);
-    assertEquals(t1.getX(1), 1.0);
-    assertEquals(t1.getY(1), 1.0);
-    assertEquals(t1.getX(2), 4.0);
-    assertEquals(t1.getY(2), 16.0);
-    assertEquals(t1.getX(3), 9.0);
-    assertEquals(t1.getY(3), 81.0);
-    assertEquals(t1.getX(4), 16.0);
-    assertEquals(t1.getY(4), 256.0);
-    assertEquals(t1.getX(5), 27.0);
-    assertEquals(t1.getY(5), 1010.0);
-    assertEquals(t1.getCount(), 6);
+        assertEquals(t1.getX(0), 0.0);
+        assertEquals(t1.getY(0), 10.0);
+        assertEquals(t1.getX(1), 1.0);
+        assertEquals(t1.getY(1), 1.0);
+        assertEquals(t1.getX(2), 4.0);
+        assertEquals(t1.getY(2), 16.0);
+        assertEquals(t1.getX(3), 9.0);
+        assertEquals(t1.getY(3), 81.0);
+        assertEquals(t1.getX(4), 16.0);
+        assertEquals(t1.getY(4), 256.0);
+        assertEquals(t1.getX(5), 27.0);
+        assertEquals(t1.getY(5), 1010.0);
+        assertEquals(t1.getCount(), 6);
     }
 }
